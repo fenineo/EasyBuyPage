77e08278562a00588512c3ab95477be709acd9ea
chenjinyj <87697868+chenjinyj@users.noreply.github.com>
2021-07-23 19:18:05 +0800

    AJAX一二三级分级
    index.html
    menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,21 @@
 
 var jq = jQuery.noConflict();
 jQuery(function(){
-	jq(".leftNav ul li").hover(
+	jq(".leftNav ul li").live("mouseenter",
 		function(){
 			jq(this).find(".fj").addClass("nuw");
 			jq(this).find(".zj").show();
 		}
-		,
+	
+	)
+
+	jq(".leftNav ul li").live("mouseleave",
+		
 		function(){
 			jq(this).find(".fj").removeClass("nuw");
 			jq(this).find(".zj").hide();
 		}
 	)
+
+
 })
